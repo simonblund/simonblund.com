1d8171595e7639965f785cd35a56ecfb92b2cdaf
Simon Blomsterlund <simon@simonblund.com>
2023-05-07 23:32:12 +0200

    Updates

---
+++ _app/immutable/nodes/4.a7b1cc85.js
@@ -0,0 +1,1 @@
+import{S as i,i as l,s as c,k as d,q as m,l as h,m as p,r as _,h as r,b as f,G as u,H as n}from"../chunks/index.fe361f2c.js";function v(o){let e,s;return{c(){e=d("div"),s=m("herre")},l(t){e=h(t,"DIV",{});var a=p(e);s=_(a,"herre"),a.forEach(r)},m(t,a){f(t,e,a),u(e,s)},p:n,i:n,o:n,d(t){t&&r(e)}}}class g extends i{constructor(e){super(),l(this,e,null,v,c,{})}}export{g as component};
