c9bc1a51a5a3c36a228ae729035a426796870c75
Simon Blomsterlund <simon@simonblund.com>
2023-05-18 01:21:04 +0300

    vite

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,9 +1,8 @@
+<script>
+	import Navbar from "../navbar.svelte";
+</script>
 <div class="col-span-9">
-    <div class="fixed top-0 left-0 right-0 bg-slate-200 col-span-full pt-4 pb-4 p-2 grid grid-cols-3 h-14 justify-items-center">
-        <a href="">Home</a>
-        <a href="">Projects</a>
-        <a href="">CV</a>
-    </div>
+    <Navbar />
     <div class="col-span-9 mt-14 bg-slate-100 grid grid-cols-9 min-h-screen">
         <slot />
     </div>
