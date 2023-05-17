5309bd84a3671f1543c8b810c2e09ba0ab9eba84
Simon Blomsterlund <simon@simonblund.com>
2023-05-18 01:13:35 +0300

    NAVBAR

--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -1,5 +1,6 @@
-<div class="fixed top-0 left-0 right-0 bg-slate-200 col-span-full pt-4 pb-4 p-2 grid grid-cols-3 h-14 justify-items-center">
-    <a href="">Home</a>
-    <a href="">Projects</a>
-    <a href="">CV</a>
+<div class="fixed top-0 left-0 right-0 bg-slate-200 col-span-full pt-4 pb-4 p-2 grid grid-cols-4 h-14 justify-items-center">
+    <a href="" class="text-slate-800">Home</a>
+    <a href="" class="text-slate-400">Projects</a>
+    <a href="" class="text-slate-400">CV</a>
+    <a href="/blog" class="text-slate-800">'Blog'</a>
 </div>
